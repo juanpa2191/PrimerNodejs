6377a3acc4081804dcc8527bef865282fc8429d6
juanpa2191 <juanpa2191@hotmail.com>
2019-05-19 20:49:29 -0500

    proyecto

---
+++ datos.js
@@ -0,0 +1,25 @@
+const opciones ={
+    nombre:{
+        alias:'n',
+        demand:true
+    },
+    matematicas:{
+        demand:true,
+        alias:'m'
+    },
+    ingles:{
+        demand: true,
+        alias: 'i'
+    },
+    programacion:{
+        demand: true,
+        alias:'p'
+    }
+};
+
+const argv = require('yargs').command('promedio','Calcular el promedio', opciones)
+.argv;
+
+console.log(argv.matematicas);
+console.log(argv);
+console.log("El promedio es: " +(argv.m+argv.i+argv.p) /3);
